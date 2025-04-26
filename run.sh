84d7abff04914a5719e65f826e827c1f1c9bca83
Aryan Palaskar <107437952+aryan-palaskar@users.noreply.github.com>
2025-04-26 13:38:52 -0700

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,16 @@
+docker run \
+    --runtime nvidia \
+    --name donkey\
+    -it\
+    --rm \
+    --privileged \
+    --net=host \
+    -e DISPLAY=$DISPLAY \
+    -v /dev/bus/usb:/dev/bus/usb \
+    --device-cgroup-rule='c 189:* rmw' \
+    --device /dev/video0 \
+    --volume='/dev/input:/dev/input' \
+    --volume='/home/jetson/.Xauthority:/root/.Xauthority:rw' \
+    --volume='/tmp/.X11-unix/:/tmp/.X11-unix' \
+    --volume='/home/jetson/projects/mycars:/home/projects/mycars' \
+    ghcr.io/ucsd-ecemae-148/donkeycontainer:base
